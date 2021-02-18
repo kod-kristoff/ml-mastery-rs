93bb97066f936f82d54bd6e23af0f99ff420f063
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-02-18 12:24:59 +0100

    feat: shuffle

--- apps/predict-owls/src/main.rs
+++ apps/predict-owls/src/main.rs
@@ -1,20 +1,23 @@
 use csv::{ReaderBuilder, WriterBuilder};
 use ndarray::{Array1, Array2};
 use ndarray_csv::{Array2Reader, Array2Writer};
+use rand::RngCore;
 use std::fs::File;
 use std::error::Error;
+use neural_network::Network;
 
 fn main() {
-    owls_and_albatrosses();
-    match owls_and_albatrosses_from_file() {
+    let mut rng = rand::thread_rng();
+    owls_and_albatrosses(&mut rng);
+    match owls_and_albatrosses_from_file(&mut rng) {
         Err(err) => {
             println!("Error: {:?}", err);
             ::std::process::exit(2);
         },
         _ => {}
     }
-    condors_and_albatrosses();
-    match condors_and_albatrosses_from_file() {
+    condors_and_albatrosses(&mut rng);
+    match condors_and_albatrosses_from_file(&mut rng) {
         Err(err) => {
             println!("Error: {:?}", err);
             ::std::process::exit(2);
@@ -23,64 +26,73 @@ fn main() {
     }
 }
 
-fn owls_and_albatrosses() {
+fn owls_and_albatrosses(rng: &mut dyn RngCore) {
     println!(">>> owls_and_albatrosses");
 
-    let (x, y) = generate_albatrosses_owls();
+    let (x, y) = generate_albatrosses_owls(rng);
 
     let now = std::time::Instant::now();
-    evaluate_perceptron(&x, &y);
+    evaluate_perceptron(&x, &y, rng);
     println!("Perceptron, elapsed time: {:.6?}", now.elapsed());
     println!("<<< owls_and_albatrosses");
 }
 
-fn owls_and_albatrosses_from_file() -> Result<(), Box<dyn Error>> {
+fn owls_and_albatrosses_from_file(rng: &mut dyn RngCore) -> Result<(), Box<dyn Error>> {
     println!(">>> owls_and_albatrosses_from_file");
 
     let (x, y) = read_albatrosses_owls()?;
 
     let now = std::time::Instant::now();
-    evaluate_perceptron(&x, &y);
+    evaluate_perceptron(&x, &y, rng);
     println!("Perceptron, elapsed time: {:.6?}", now.elapsed());
     println!("<<< owls_and_albatrosses_from_file");
     Ok(())
 }
 
-fn condors_and_albatrosses() {
+fn condors_and_albatrosses(rng: &mut dyn RngCore) {
     println!(">>> condors_and_albatrosses");
 
-    let (x, y) = generate_albatrosses_condorss();
+    let (x, y) = generate_albatrosses_condorss(rng);
 
     let now = std::time::Instant::now();
-    evaluate_perceptron(&x, &y);
+    evaluate_perceptron(&x, &y, rng);
     println!("Perceptron, elapsed time: {:.6?}", now.elapsed());
     println!("<<< condors_and_albatrosses");
 }
 
-fn condors_and_albatrosses_from_file() -> Result<(), Box<dyn Error>> {
+fn condors_and_albatrosses_from_file(rng: &mut dyn RngCore) -> Result<(), Box<dyn Error>> {
     println!(">>> condors_and_albatrosses_from_file");
 
     let (x, y) = read_albatrosses_condorss()?;
 
     let now = std::time::Instant::now();
-    evaluate_perceptron(&x, &y);
+    evaluate_perceptron(&x, &y, rng);
     println!("Perceptron, elapsed time: {:.6?}", now.elapsed());
     println!("<<< condors_and_albatrosses_from_file");
     Ok(())
 }
 
-fn generate_albatrosses_owls() -> (Array2<f64>, Array1<i32>) {
+fn generate_albatrosses_owls(rng: &mut dyn RngCore) -> (Array2<f64>, Array1<i32>) {
     let mut x = Array2::<f64>::zeros((200, 2));
     let mut y = Array1::<i32>::zeros(200);
-    let (a_x, a_y) = species_generator(9000.0, 800.0, 300.0, 20.0, 100, 1);
-    let (o_x, o_y) = species_generator(1000.0, 200.0, 100.0, 15.0, 100, -1);
+    let (a_x, a_y) = species_generator(9000.0, 800.0, 300.0, 20.0, 100, 1, rng);
+    let (o_x, o_y) = species_generator(1000.0, 200.0, 100.0, 15.0, 100, -1, rng);
     x.slice_mut(ndarray::s![0..100, ..]).assign(&a_x);
     x.slice_mut(ndarray::s![100.., ..]).assign(&o_x);
     y.slice_mut(ndarray::s![0..100]).assign(&a_y);
     y.slice_mut(ndarray::s![100..]).assign(&o_y);
     // let x = ndarray::stack_new_axis![ndarray::Axis(0), a_x, o_x];
     
-    (x, y)
+    let shuffle = rand::seq::index::sample(rng, 200, 200);
+
+    let mut x_shuffled = Array2::<f64>::zeros((200, 2));
+    let mut y_shuffled = Array1::<i32>::zeros(200);
+
+    for (j, i) in shuffle.iter().zip(0..200) {
+        x_shuffled.row_mut(i).assign(&x.row(j));
+        y_shuffled[i] = y[j];
+    }
+    (x_shuffled, y_shuffled)
 }
 
 fn read_albatrosses_owls() -> Result<(Array2<f64>, Array1<i32>), Box<dyn Error>> {
@@ -98,18 +110,27 @@ fn read_albatrosses_owls() -> Result<(Array2<f64>, Array1<i32>), Box<dyn Error>>
     Ok((x, y))
 }
 
-fn generate_albatrosses_condorss() -> (Array2<f64>, Array1<i32>) {
+fn generate_albatrosses_condorss(rng: &mut dyn RngCore) -> (Array2<f64>, Array1<i32>) {
     let mut x = Array2::<f64>::zeros((200, 2));
     let mut y = Array1::<i32>::zeros(200);
-    let (a_x, a_y) = species_generator(9000.0, 800.0, 300.0, 20.0, 100, 1);
-    let (o_x, o_y) = species_generator(12000.0, 1000.0, 290.0, 15.0, 100, -1);
+    let (a_x, a_y) = species_generator(9000.0, 800.0, 300.0, 20.0, 100, 1, rng);
+    let (o_x, o_y) = species_generator(12000.0, 1000.0, 290.0, 15.0, 100, -1, rng);
     x.slice_mut(ndarray::s![0..100, ..]).assign(&a_x);
     x.slice_mut(ndarray::s![100.., ..]).assign(&o_x);
     y.slice_mut(ndarray::s![0..100]).assign(&a_y);
     y.slice_mut(ndarray::s![100..]).assign(&o_y);
     // let x = ndarray::stack_new_axis![ndarray::Axis(0), a_x, o_x];
+    let shuffle = rand::seq::index::sample(rng, 200, 200);
+
+    let mut x_shuffled = Array2::<f64>::zeros((200, 2));
+    let mut y_shuffled = Array1::<i32>::zeros(200);
+
+    for (j, i) in shuffle.iter().zip(0..200) {
+        x_shuffled.row_mut(i).assign(&x.row(j));
+        y_shuffled[i] = y[j];
+    }
+    (x_shuffled, y_shuffled)
     
-    (x, y)
 }
 
 fn read_albatrosses_condorss() -> Result<(Array2<f64>, Array1<i32>), Box<dyn Error>> {
@@ -128,14 +149,14 @@ fn read_albatrosses_condorss() -> Result<(Array2<f64>, Array1<i32>), Box<dyn Err
 }
 
 
-fn species_generator(mu1: f64, sigma1: f64, mu2: f64, sigma2: f64, n_samples: usize, target: i32) -> (Array2<f64>, Array1<i32>) {
+fn species_generator(mu1: f64, sigma1: f64, mu2: f64, sigma2: f64, n_samples: usize, target: i32, rng: &mut dyn RngCore) -> (Array2<f64>, Array1<i32>) {
     use ndarray_rand::RandomExt;
     use ndarray_rand::rand_distr::Normal;
 
     let normal1 = Normal::new(mu1, sigma1).unwrap();
     let normal2 = Normal::new(mu2, sigma2).unwrap();
-    let values1 = Array1::<f64>::random(n_samples, normal1);
-    let values2 = Array1::<f64>::random(n_samples, normal2);
+    let values1 = Array1::<f64>::random_using(n_samples, normal1, rng);
+    let values2 = Array1::<f64>::random_using(n_samples, normal2, rng);
     let x = ndarray::stack![ndarray::Axis(1), values1, values2];
     assert_eq!(x.shape(), &[n_samples, 2]);
     let y = Array1::<i32>::from_elem(n_samples, target);
@@ -143,10 +164,10 @@ fn species_generator(mu1: f64, sigma1: f64, mu2: f64, sigma2: f64, n_samples: us
 
 }
 
-fn evaluate_perceptron(x: &Array2<f64>, y: &Array1<i32>) {
+fn evaluate_perceptron(x: &Array2<f64>, y: &Array1<i32>, rng: &mut dyn RngCore) {
     let eta = 0.01;
     let n_iter = 200;
-    let network = neural_network::Network::fit(x, y, eta, n_iter);
+    let network = Network::perceptron_fit(x, y, eta, n_iter, rng);
     let y_pred = network.predict(x);
     let num_correct: i64 = y.iter().zip(y_pred.iter()).map(|(y, yp)| {
         if y == yp {
--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,43 +1,39 @@
-use ndarray::{Array0, Array1, Array2, ArrayView1, ArrayView2};
+use ndarray::{Array1, Array2, ArrayView1, ArrayView2};
+use rand::RngCore;
 
 pub struct Network {
     weights: Array1<f64>,
     bias: f64,
-    errors: Array1<i64>,
 }
 
 impl Network {
-    pub fn fit(x: &Array2<f64>, y: &Array1<i32>, eta: f64, n_iter: usize) -> Self {
-        let (mut weights, mut bias) = random_weights(x);
-        let mut errors = Array1::<i64>::zeros(n_iter);
-        for j in 0..n_iter {
+    pub fn perceptron_fit(x: &Array2<f64>, y: &Array1<i32>, eta: f64, n_iter: usize, rng: &mut dyn RngCore) -> Self {
+        let (mut weights, mut bias) = random_weights(x, rng);
+        for _ in 0..n_iter {
             for i in 0..y.len() {
                 let xi = x.slice(ndarray::s![i..(i+1), ..]);
                 // println!("predicy(xi) = {:?}", do_predict(&xi.t(), &weights));
                 let target = y[i];
                 
-                let delta = eta * (target - do_predict(&xi, &weights, bias)[0]) as f64;
+                let delta = eta * (target - do_predict(&xi, &weights.view(), bias)[0]) as f64;
                 // for k in 1..weights.len() {
                  
                 // weights[k] += delta * xi[[0, k-1]];
                 // }
                 weights += &(delta * &xi.row(0));
                 bias += delta;
-                if !approx::abs_diff_eq!(delta, 0.0) {
-                    errors[j] += 1;
-                }
             }
         }
-        Self { weights, bias, errors }
+        Self { weights, bias }
     }
 
     pub fn predict(&self, x: &Array2<f64>) -> Array1<i32> {
-        do_predict(&x.view(), &self.weights, self.bias)
+        do_predict(&x.view(), &self.weights.view(), self.bias)
     }
 }
 
-fn do_predict(x: &ArrayView2<f64>, w: &Array1<f64>, bias: f64) -> Array1<i32> {
-    net_input(x, &w.view(), bias).map(|x| {
+fn do_predict(x: &ArrayView2<f64>, w: &ArrayView1<f64>, bias: f64) -> Array1<i32> {
+    net_input(x, w, bias).map(|x| {
         if *x < 0.0 {
             -1
         } else {
@@ -51,21 +47,43 @@ fn net_input(x: &ArrayView2<f64>, w: &ArrayView1<f64>, bias: f64) -> Array1<f64>
 }
 
 
-fn random_weights(x: &Array2<f64>) -> (Array1<f64>, f64) {
+fn random_weights(x: &Array2<f64>, rng: &mut dyn RngCore) -> (Array1<f64>, f64) {
     use ndarray_rand::RandomExt;
     use ndarray_rand::rand_distr::Normal;
+    use rand::distributions::Distribution;
 
     let normal = Normal::new(0., 0.01).unwrap();
-    let weights = Array1::<f64>::random(x.shape()[1], normal);
-    let bias = Array1::<f64>::random(1, normal)[0];
+    let weights = Array1::<f64>::random_using(x.shape()[1], normal, rng);
+    let bias = normal.sample(rng);
     (weights, bias)
-    // Array1::<f64>::zeros(1 + x.shape()[1])
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    mod random_weights {
+        use super::*;
+        use rand_chacha::ChaCha8Rng;
+        use rand::SeedableRng;
+
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+            let x = Array2::<f64>::zeros((4, 5));
+
+            let (weights, bias) = random_weights(&x, &mut rng);
+
+            assert_eq!(weights.shape(), &[x.shape()[1]]);
+            assert_relative_eq!(bias, -0.010646159174430792);
+            assert_relative_eq!(weights[0], 0.013776972067507914);
+        }
     }
 }
