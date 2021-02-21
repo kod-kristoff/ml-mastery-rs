ea2e872fe30280482b0350f598528388d59e65d6
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-02-21 19:47:37 +0100

    feat: impl fit

--- libs/neural-network/src/mlp.rs
+++ libs/neural-network/src/mlp.rs
@@ -21,6 +21,36 @@ impl MlpNetwork {
 
         Self { layers }
     }
+
+    pub fn fit(x: &Array2::<f64>, y: &Array2::<f64>, topology: &[usize], iterations: usize, eta: f64, rng: &mut dyn RngCore) -> Self {
+        let mut network = Self::random(topology, rng);
+
+        for _ in 0..iterations {
+            // Forward-propagation
+            let s1 = network.layers[0].propagate(x);
+            let s2 = network.layers[1].propagate(&s1);
+
+            // Back-propagation
+            let delta2 = (&s2 - y)*&s2*(1. - &s2);
+            let w2_gradients = s1.t().dot(&delta2);
+            network.layers[1].weights -= &(w2_gradients * eta);
+            network.layers[1].bias -= delta2.sum() * eta;
+
+            let delta1 = delta2.dot(&network.layers[1].weights.t()) * &s1 * (1. - &s1);
+            let w1_gradients = x.t().dot(&delta1);
+            network.layers[0].weights -= &(w1_gradients * eta);
+            network.layers[0].bias -= delta1.sum() * eta;
+        }
+
+        network
+    }
+
+    pub fn propagate(&self, x: &Array2<f64>) -> Array2<f64> {
+        let x = x.clone();
+        self.layers
+            .iter()
+            .fold(x, |x, layer| layer.propagate(&x)) 
+    }
 }
 
 impl Layer {
@@ -43,6 +73,7 @@ impl Layer {
 mod tests {
     use super::*;
     use approx::assert_relative_eq;
+    use ndarray::arr2;
 
     mod random {
         use super::*;
@@ -62,11 +93,47 @@ mod tests {
 
             assert_relative_eq!(network.layers[0].bias[[0, 0]], 0.06738395137652944);
         }
+
+        #[test]
+        fn fit() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let x = arr2(
+                &[[0., 0.],
+                  [0., 1.],
+                  [1., 0.],
+                  [1., 1.]]
+            );
+            let y = arr2(
+                &[[0.],
+                  [1.],
+                  [1.],
+                  [0.]]
+            );
+            let iterations = 10;
+            let eta = 0.01;
+            let network = MlpNetwork::fit(&x, &y, &[2, 3, 1], iterations, eta, &mut rng);
+
+            assert_eq!(network.layers.len(), 2);
+
+            assert_relative_eq!(
+                &network.layers[0].weights,
+                &arr2(
+                    &[[-0.07648684679919386, -0.048877017544423015, -0.08020791605296769],
+ [-0.09868510742094942, -0.0476596245401859, -0.036130561247895]])
+            );
+            assert_relative_eq!(
+                &network.layers[1].weights,
+                &arr2(
+                    &[[0.03266122626760181],
+                      [-0.0165584307986786],
+                      [0.018798716274266193]]
+                )
+            );
+        }
     }
 
     mod propagate {
         use super::*;
-        use ndarray::arr2;
 
         #[test]
         fn layer_propagate() {
@@ -91,6 +158,45 @@ mod tests {
             let res = layer.propagate(&x);
 
             assert_eq!(res.shape(), &[2, 3]);
+            assert_relative_eq!(res[[0, 0]], 1./(1. + (-8_f64).exp()));
+            assert_relative_eq!(res[[1, 0]], 1./(1. + (-7_f64).exp()));
+        }
+
+        #[test]
+        fn network_propagate() {
+            let weights1 = arr2(
+                &[[1., 2., 3.],
+                  [3., 1., 2.],]
+            );
+            let bias1 = arr2(&[
+                [1., 2., 3.]
+            ]);
+            let weights2 = arr2(
+                &[[1., 2.],
+                  [3., 1.],
+                  [3., 2.]]
+            );
+            let bias2 = arr2(&[
+                [1., 2.]
+            ]);
+            let layers = vec![
+                Layer { weights: weights1, bias: bias1 },
+                Layer { weights: weights2, bias: bias2 },
+            ];
+
+            let network = MlpNetwork { layers };
+            let x = arr2(
+                &[[1., 2.],
+                  [3., 1.]]);
+            let res = network.propagate(&x);
+
+            assert_eq!(res.shape(), &[2, 2]);
+            assert_relative_eq!(
+                res, 
+                network.layers[1].propagate(
+                    &network.layers[0].propagate(&x)
+                )
+            );
         }
     }
 }
