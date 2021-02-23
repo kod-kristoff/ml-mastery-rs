3a877f821cb3ea21d144d881a2ce4e117d38a9cf
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-02-23 22:11:16 +0100

    feat: add make_circles stub

--- libs/dataset-gen/src/lib.rs
+++ libs/dataset-gen/src/lib.rs
@@ -1,7 +1,28 @@
+pub fn make_circles(factor: f64) -> ((), ()) {
+    if factor < 0.0 || factor > 1.0 {
+        panic!("'factor' has to be between 0 and 1, got {}", factor);
+    }
+    ((), ())
+}
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use super::*;
+
+    mod make_circles {
+        use super::*;
+
+        #[test]
+        #[should_panic]
+        fn factor_below_0_panics() {
+            let factor = -1.0;
+            let (x, y) = make_circles(factor);
+        }
+
+        #[test]
+        #[should_panic]
+        fn factor_above_1_panics() {
+            let factor = 1.01;
+            let (x, y) = make_circles(factor);
+        }
     }
 }
