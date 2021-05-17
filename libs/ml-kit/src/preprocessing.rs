d1e7332e9b23648621bd0120f3abcbf60dd62a89
Kristoffer Andersson <kod.kristoff@gmail.com>
2021-05-17 16:54:03 +0200

    feat: work

---
+++ libs/ml-kit/src/preprocessing.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+pub struct LabelEncoder {
+    map: HashMap<usize, usize>,
+}
+
+impl LabelEncoder {
+    pub fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
+    }
+    
+    pub fn fit(&mut self, data: &[usize]) {
+        for d in data {
+            if !self.map.contains_key(d) {
+                self.map.insert(*d, self.map.len());
+            }
+        }
+    } 
+
+    pub fn classes(&self) -> Vec<usize> {
+        self.map.keys().map(|v| *v).collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    mod label_encoder {
+        use super::*;
+
+        #[test]
+        fn fit_creates_classes() {
+            let mut label_encoder = LabelEncoder::new();
+
+            label_encoder.fit(&[1, 2, 2, 6]);
+
+            assert_eq!(label_encoder.classes(), &[2, 6, 1]);
+        }
+    }
+}
